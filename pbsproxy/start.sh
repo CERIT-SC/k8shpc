5d93da4bf056ae26a5d6f802424e6f10853d7ba8
Lukas Hejtmanek <xhejtman@gmail.com>
2022-10-29 17:17:58 +0200

    PBSPROXY: use secrets for ssh keys

--- pbsproxy/start.sh
+++ pbsproxy/start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-echo -e "\n" | ssh-keygen -q -N ''
+if kubectl get secret -n ${NAMESPACE} sshkey &> /dev/null; then
+  export ssh_key=`kubectl get secret -n ${NAMESPACE} sshkey -o jsonpath='{.data.ssh-privatekey}' | base64 -d`
+else 
+  echo -e "\n" | ssh-keygen -q -N ''
+  kubectl create secret generic sshkey --from-file=ssh-privatekey=/home/funnelworker/.ssh/id_rsa --from-file=ssh-publickey=/home/funnelworker/.ssh/id_rsa.pub -n ${NAMESPACE}
+  export ssh_key=`cat /home/funnelworker/.ssh/id_rsa`
+fi
 
-cp /home/funnelworker/.ssh/id_rsa.pub /home/funnelworker/.ssh/authorized_keys
-chmod 0600 /home/funnelworker/.ssh/authorized_keys
+kubectl get secret -n ${NAMESPACE} sshkey -o jsonpath='{.data.ssh-publickey}' | base64 -d > /home/funnelworker/.ssh/authorized_keys && chmod 0600 /home/funnelworker/.ssh/authorized_keys 
 
 set | grep 'PROXY\|KUBERNETES' | sed -e 's/^/export /' >> /home/funnelworker/.bashrc; 
 /usr/sbin/sshd
